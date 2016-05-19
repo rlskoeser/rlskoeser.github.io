dc8b58543983b89bfec3872f23ba8b497786f827
rlskoeser <rebecca.s.koeser@emory.edu>
2016-05-19 16:00:13 -0400

    Fix webmention plural/singular; move into separate js file

---
+++ assets/js/_webmentions.js
@@ -0,0 +1,53 @@
+/* fetch and display web mentions for a specified url (i.e., current page) */
+function webmentions(url) {
+  $.getJSON("http://webmention.io/api/mentions?jsonp=?", {
+    target: url
+  }, function(data){
+    // if there are no links, nothing to do
+    if (data.links.length == 0) {
+      return;
+    }
+
+    // add content to the webmentions div on post & update pages
+    var div = $(".webmentions");
+    var details = $('<div class="mention-details"/>');
+    var mention, content;
+    var plural = (data.links.length == 1 ? '' : 's');
+    div.append($('<span>' + data.links.length + ' mention' + plural + ': </span>'));
+
+    var activity = {}, type;
+    for (var i = 0; i < data.links.length; i++) {
+      type = data.links[i].activity.type;
+      if (! (type in activity)) {
+        activity[type] = 0;
+      }
+      activity[type]++;
+      // create a div for each mention
+      mention = $('<div/>').attr('class', type);
+      content = $(data.links[i].activity.sentence_html);
+      $.each(content, function(i, el) {
+        // sentence description often includes full url to this page
+        // strip it out if present, but add other pieces to a detail div
+        if (type != 'link' && $(el).attr('href') == "{{ site.url }}{{ page.url }}") {
+          return;
+        }
+        mention.append(el);
+      })
+      details.append(mention);
+    }
+
+    var types = Object.keys(activity), type;
+    for (var i = 0; i < types.length; i++) {
+      type = types[i];
+      plural = activity[type] == 1 ? '' : 's';
+      div.append($('<span>' + activity[type] + ' ' + type + plural + '</span>').attr('class', types[i]));
+
+      if (i != types.length -1 ) {
+        div.append(', ');
+      }
+    }
+    div.append(details);
+
+  });
+
+}
