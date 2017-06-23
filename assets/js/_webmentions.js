e7e76e20c0aaad516de9c4d28844446bf43a38d1
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-06-23 14:30:12 -0400

    Revised webmention display: show external links

--- assets/js/_webmentions.js
+++ assets/js/_webmentions.js
@@ -6,24 +6,68 @@ function webmentions(url) {
   console.log(current_url);
 
   // add content to the webmentions div on post & update pages
-  var div = $(".webmentions");
-  var details = $('<div class="mention-details"/>');
-  div.append(details);
+  var details = $('.mention-details'),
+    summary = $('.mention-summary');
+
+  function cleanup() {
+    // bridgy mentions include current page url, which is very redundant
+    // and hard to read; remove it for likes, replies, and retweets
+    details.find('.like a[href$="' + window.location.pathname + '"]').remove();
+    details.find('.repost a[href$="' + window.location.pathname + '"]').remove();
+    details.find('.reply a[href$="' + window.location.pathname + '"]').remove();
+  }
+
+  function summarize_mentions() {
+      var activity = {}, type,
+        mentions = details.find('div');
+        summary.children().remove();
+      summary.append($('<span>' + mentions.length + ' mentions:</span>').attr('class', 'total'));
+
+      $.each(mentions, function(i, el) {
+        type = $(el).attr('class');
+        // count instance of activity
+        if (! (type in activity)) {
+          activity[type] = 0;
+        }
+        activity[type]++;
+      });
+      console.log(activity);
+
+    var types = Object.keys(activity), type, count, plural;
+    for (var i = 0; i < types.length; i++) {
+      type = types[i];
+      count = activity[type];
+      plural = (count == 1 ? '' : 's');
+      if (type == 'reply' && count != 1) {
+          type = 'replie';
+      }
+      summary.append($('<span>' + count + ' ' + type + plural + '</span>').attr('class', types[i]));
+    }
+
+    cleanup();
+  }
 
   function display_mention(data) {
-      var type = data.activity.type;
       // create a div for each mention
-      mention = $('<div/>').attr('class', type);
+      if ($('#' + data.id).length) {
+        console.log('id already exists ' + data.id);
+        return;
+      }
+      mention = $('<div/>').attr('class', data.activity.type).attr('id', data.id);
       content = $(data.activity.sentence_html);
+      console.log(content);
+      console.log(data.data.content);
       $.each(content, function(i, el) {
-        // sentence description often includes full url to this page
-        // strip it out if present, but add other pieces to a detail div
-        if (type != 'link' && $(el).attr('href') == "{{ site.url }}{{ page.url }}") {
-          return;
-        }
         mention.append(el);
       })
+      if (data.activity.type == 'link' || data.activity.type == 'reply') {
+        console.log(data);
+        var srcdiv = $('<div/>').attr('class', 'source');
+        srcdiv.append($('<a/>').attr('href', data.data.url).text(data.data.url));
+        mention.append(srcdiv);
+      }
       details.append(mention);
+
   }
 
 
@@ -33,33 +77,45 @@ function webmentions(url) {
       console.log('loaded local json');
       // console.log(data);
       for (var i = 0; i < data.length; i++) {
-        console.log(data[i]);
-        console.log(data[i].target);
+        // console.log(data[i]);
+        // console.log(data[i].target);
         if (data[i].target.endsWith(window.location.pathname)) {
-          console.log(data[i]);
+          // console.log(data[i]);
           display_mention(data[i]);
         }
-      }
 
+      }
+      summarize_mentions();
   });
 
   $.getJSON("https://webmention.io/api/mentions?jsonp=?", {
     target: url
   }, function(data){
-    // if there are no links, nothing to do
-    if (data.links.length == 0) {
-      return;
-    }
+      // if there are no links, nothing to do
+      if (data.links.length == 0) {
+        return;
+      }
+      // add content to the webmentions div on post & update pages
+      var details = $('.mention-details');
 
-    // add content to the webmentions div on post & update pages
-    var div = $(".webmentions");
-    var details = $('<div class="mention-details"/>');
+      for (var i = 0; i < data.links.length; i++) {
+        display_mention(data.links[i]);
+      }
+
+      summarize_mentions();
+      /*
+
+
     var mention, content;
     var plural = (data.links.length == 1 ? '' : 's');
     div.append($('<span>' + data.links.length + ' mention' + plural + ': </span>'));
 
     var activity = {}, type;
     for (var i = 0; i < data.links.length; i++) {
+      display_mention(data[i]);
+
+
+
       type = data.links[i].activity.type;
       if (! (type in activity)) {
         activity[type] = 0;
@@ -92,8 +148,9 @@ function webmentions(url) {
       if (i != types.length -1 ) {
         div.append(', ');
       }
-    }
-    div.append(details);
+
+    } */
+    // div.append(details);
 
   });
 
