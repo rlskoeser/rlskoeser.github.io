19fd18ee1a18789ecbb7f4ca006d808b70902bed
rlskoeser <rlskoeser@gmail.com>
2023-06-29 15:21:55 -0400

    Update migration script to copy featured images into article bundle

--- utils/ConvertToHugo.py
+++ utils/ConvertToHugo.py
@@ -4,13 +4,20 @@
 import os
 import re
 import yaml
+import shutil
 from datetime import datetime, date
 import argparse
+import requests
 
 __author__ = 'coderzh'
-# with modifications
+# with modifications by rlskoeser
 
 # pip install pyyaml==5.4.1
+
+# to run: takes an input post dir and an output dir
+# for rlskoeser site jekyll blog posts:
+# pipenv run utils/ConvertToHugo.py ~/workarea/github/rlskoeser.github.io/_posts content/posts/
+
 
 class MyDumper(yaml.Dumper):
     def increase_indent(self, flow=False, indentless=False):
@@ -18,6 +25,10 @@ class MyDumper(yaml.Dumper):
 
 content_regex = re.compile(r'---([\s\S]*?)---([\s\S]*)')
 
+## revise:
+# - convert to directory, move featured image into directory, move thumb into directory
+# - convert multiple authors to authors: list
+# - create author data files manually (report in the conversion script)
 
 def convert_front_matter(front_data, post_date, url, show_toc=False):
     front_data['url'] = url
@@ -40,7 +51,7 @@ def convert_front_matter(front_data, post_date, url, show_toc=False):
         # del front_data['created']
 
     if show_toc:
-        front_data['toc'] = 'True' # if show_toc else 'False'∫∫
+        front_data['showTableOfContents'] = True # if show_toc else 'False'∫∫
 
     # for updates, rname url to slug
     if 'publish_to_twitter' in front_data:
@@ -77,6 +88,23 @@ def convert_front_matter(front_data, post_date, url, show_toc=False):
         if not front_data['image']:
             del front_data['image']
 
+    # if second author is specified without first, specify show author (= include default author)
+    if 'second_author' in front_data and not 'author' in front_data:
+        front_data['showAuthor'] = True
+        front_data["authors"] = [front_data['second_author']]
+        del front_data["second_author"]
+
+    # if author is specified, set in list
+    if 'author' in front_data:
+        front_data['authors']  = [front_data['author']]
+        del front_data["author"]
+
+    # one case of third author; add to list after second
+    if 'third_author' in front_data:
+        front_data["authors"].append(front_data['third_author'])
+        del front_data["third_author"]
+
+
 
 replace_regex_list = [
 #    (re.compile(r'^```(.*?)\n(.*?)\n```', re.DOTALL), r'{{< highlight \1 >}}\n\2\n{{< /highlight >}}'),
@@ -85,7 +113,8 @@ replace_regex_list = [
     (re.compile(r'\{%\sinclude\sfigure\.html\s(.*)\s%\}'), r'{{< figure \1 >}}'),
     (re.compile(r'\{%\sinclude\s_toc\.html\s%\}'), r''),
     (re.compile(r'\{%\shighlight\s(.*)%\}'), r'{{< highlight \1 >}}'),
-    (re.compile(r'\{%\sendhighlight\s%\}'), r'{{< / highlight >}}'),
+    (re.compile(r'\{%\s+endhighlight\s%\}'), r'{{< / highlight >}}'),
+    # convert inline class syntax; NOTE: only works on blocks, probably requires manual cleanup
     (re.compile(r'\{: .([^}]*)\}'), r'{.\1}'),
 ]
 
@@ -118,6 +147,7 @@ def parse_from_filename(filename):
     if m:
         slug = m.group(2)
         post_date = datetime.strptime(m.group(1), '%Y-%m-%d')
+        # return post_date, slug
         return post_date, '/%s/%s/' % (post_date.strftime('%Y/%m/%d'), slug)
     return None, '/' + slug
 
@@ -125,6 +155,10 @@ def parse_from_filename(filename):
 def convert_post(file_path, out_dir):
     filename = os.path.basename(file_path)
     post_date, url = parse_from_filename(filename)
+    # out_dir is _base_ outdir; one directory for post so we can use bundles
+    base_out_dir = out_dir
+    out_dir = os.path.join(out_dir, os.path.splitext(filename)[0])
+    print('out_dir ', out_dir)
 
     content = ''
     with open(file_path, 'r') as f:
@@ -151,7 +185,9 @@ def convert_post(file_path, out_dir):
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
 
-    out_file_path = os.path.join(out_dir, filename)
+    # out_file_path = os.path.join(out_dir, filename)
+    # create post as an index file within the article folder
+    out_file_path = os.path.join(out_dir, "index.md")
 
     show_toc = '{% include _toc.html %}' in m.group(2)
 
@@ -159,7 +195,65 @@ def convert_post(file_path, out_dir):
     body_text = convert_body_text(m.group(2))
     write_out_file(front_data, body_text, out_file_path)
 
-    return True
+    # copy image files into post bundle
+    if "featured_image" in front_data:
+        featured_image = front_data["featured_image"]
+        # calculate local filename
+        featured_image_dest = os.path.basename(featured_image)
+        if "banner" in featured_image_dest:
+            featured_image_dest = featured_image_dest.replace("banner", "featured")
+        else:
+            base, ext = os.path.splitext(featured_image_dest)
+            featured_image_dest = "%s_featured%s" % (base, ext)  # ext includes .
+
+        featured_image_dest = os.path.join(out_dir, featured_image_dest)
+        print("** featured image dest %s" % featured_image_dest)
+
+        # copy local file
+        if featured_image.startswith("/"):
+            # jekyll post images have been copied into local hugo static directory
+            featured_image_src = os.path.join("static", featured_image.lstrip("/"))
+
+            print("copy %s %s" % (featured_image_src, featured_image_dest))
+            shutil.copyfile(featured_image_src, featured_image_dest)
+        else:
+            response = requests.get(featured_image)
+            if response.status_code == requests.codes.ok:
+                with open(featured_image_dest, 'wb') as outfile:
+                    for chunk in response.iter_content(chunk_size=128):
+                        outfile.write(chunk)
+            else:
+                print("*** error downloading %s" % featured_image)
+
+
+    if "thumbnail_image" in front_data:
+        thumbnail_img = front_data["thumbnail_image"]
+        print('*** thumbnail image %s' % thumbnail_img)
+        thumbnail_dest = os.path.basename(thumbnail_img)
+        if "thumbnail" not in thumbnail_dest:
+            if "thumb" in thumbnail_dest:
+                thumbnail_dest = thumbnail_dest.replace("thumb", "thumbnail")
+            else:
+                base, ext = os.path.splitext(thumbnail_dest)
+                thumbnail_dest = "%s_thumbnail%s" % (base, ext)  # ext includes .
+        thumbnail_dest = os.path.join(out_dir, thumbnail_dest)
+        print("** thumbnail image dest %s" % thumbnail_dest)
+
+        # for local images, copy from image to post bundle dir
+        if thumbnail_img.startswith("/"):
+            # jekyll post images have been copied into local hugo static directory
+            thumbnail_src = os.path.join("static/", thumbnail_img.lstrip("/"))
+            print("copy %s %s" % (thumbnail_src, thumbnail_dest))
+            shutil.copyfile(thumbnail_src, thumbnail_dest)
+
+        else:
+            response = requests.get(thumbnail_img)
+            if response.status_code == requests.codes.ok:
+                with open(thumbnail_dest, 'wb') as outfile:
+                    for chunk in response.iter_content(chunk_size=128):
+                        outfile.write(chunk)
+            else:
+                print("*** error downloading %s" % thumbnail_img)
 
 
 def convert(src_dir, out_dir):
@@ -193,4 +287,3 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     convert(os.path.abspath(args.src_dir), os.path.abspath(args.out_dir))
-
