2d78303c3e6e52e1e68a1ae1cfdd113ab9c36f09
rlskoeser <rebecca.s.koeser@princeton.edu>
2024-06-07 16:28:03 -0400

    Clean up script for fetching web mention data

--- scripts/webmentions.py
+++ scripts/webmentions.py
@@ -1,57 +1,65 @@
 #!/usr/bin/env python
+"""
+python script to fetch webmentions and save as data files
+
+mentions are grouped by top-level url portion and saved to files
+by group
+
+setup:
+- pip install requests
+- set webmention api token as environment variable WEBMENTION_API_TOKEN
+"""
 
 import argparse
-from collections import defaultdict
 import json
-import requests
+import os
+from collections import defaultdict
+from datetime import datetime
 from urllib.parse import urlparse
+from pathlib import Path
+
+import requests
 
 
 def fetch_mentions():
-    # get these from config file
-    domain = 'rlskoeser.github.io'
-    token = 'z7GVt5ozMItgcuqjLQ-Alg'
-    webmention_api_url = 'https://webmention.io/api/mentions'
+    domain = "rlskoeser.github.io"
+    token = os.environ.get("WEBMENTION_API_TOKEN")
+    webmention_api_url = "https://webmention.io/api/mentions"
 
-    link_data = defaultdict(list)
+    webmention_data_dir = Path("data/webmentions/")
+    mention_data = defaultdict(list)
 
+    # API results are paginated, with link to next page; iterate until we get to the end
     page = 0
     while True:
-        print('getting page %s' % page)
-        response = requests.get(webmention_api_url, params={
-            'domain': domain, 'token': token, 'perPage': 50, 'page': page
-        })
+        # GET https://webmention.io/api/mentions.jf2?domain=indiewebcamp.com&token=xxxxx&since=2017-06-01T10:00:00-0700
+        params = {"domain": domain, "token": token, "perPage": 50, "page": page}
+        # NOTE: supports a 'since' parameter to fetch mentions since last check
+        response = requests.get(webmention_api_url, params=params)
         data = response.json()
         # stop iterating over api results when we run out of links
-        if not data['links']:
+        if not data["links"]:
             break
 
         # iterate over mentions and store
-        for mention in data['links']:
+        for mention in data["links"]:
             target_url = urlparse(mention["target"])
-            site_section = target_url.path.strip('/').split('/')[0] or 'home'
-            print('%s; section %s' % (mention["target"], site_section))
-            link_data[site_section].append(mention)
+            site_section = target_url.path.strip("/").split("/")[0] or "home"
+            mention_data[site_section].append(mention)
 
-
+        # check for next page of results
         page += 1
 
-    for section in link_data.keys():
-        print(section)
-        with open('assets/json/webmentions/%s.json' % section, 'w') as outfile:
-            json.dump(link_data[site_section], outfile, indent=4)
+    for section, mentions in mention_data.items():
+        # NOTE: could probably skip home, updates;
+        # not currently displaying mentions for tag pages
+        print(
+            f"Saving {len(mentions)} mention{'' if len(mentions) == 1 else 's'} for {section}"
+        )
+        section_datafile = webmention_data_dir / f"{section}.json"
+        with section_datafile.open("w") as outfile:
+            json.dump(mentions, outfile, indent=4)
 
 
-    # https://webmention.io/api/mentions?domain=rlskoeser.github.io&token=&perPage=20&page=7
-
-
-if __name__ == '__main__':
-    print('main')
-    parser = argparse.ArgumentParser(description='Do WebMentions stuff')
-    parser.add_argument('mode', choices=['fetch'],
-        help='fetch mentions')
-    args = parser.parse_args()
-    print(args)
-    if args.mode == 'fetch':
-        fetch_mentions()
-
+if __name__ == "__main__":
+    fetch_mentions()
